9009d672efbc973bc17bfeb6fe9c3d4a1a9fc174
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2020-05-09 16:36:57 +0900

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,27 +1,31 @@
 class VMConfig {
-    # you input WindowsOS　you wants into variable,if you don't know that,
+    # you input ostype you wants into variable,if you don't know that,
     # you execute commands `VBoxmanagement list ostypes`,and you use nearest ID in displayed terminal. 
     # ex) $VMOStype = "Windows2019_64"
-    [String] $VMOStype = "Windows2019_64"
+    [String] $VMOStype = 
 
     # Virtual Machine name. It is easy to understand for you.
     # ex) $VMName = "WindowsServer"
-    [String] $VMName = "WindowsServer"
+    [String] $VMName = 
 
     # you input memory to use Virtual Machine. Unit is M. If you want to GUI, it is necessary at least 4096.
-    [int] $VMMemory = 8192
+    # ex) [int] $VMMemory = 8192
+    [int] $VMMemory = 
 
     # you input Virtual Machine Vram. Unit is M. Upper limit for Vram is 256.
-    [int] $VMRam = 256
+    # ex) [int] $VMRam = 256
+    [int] $VMRam = 
 
     # Virtual Machine is using Number of Cpu.
-    [int] $VMCpus = 4
+    # ex) [int] $VMCpus = 4
+    [int] $VMCpus = 
 
     # you input Virtual Disk size. Unit is M.
-    [int] $VDISize = 80000
+    # ex) [int] 8192
+    [int] $VDISize =  
 
     # you input iso path. ex) ~/Download/WindowsServer.iso
     # ex) $isopath = "~/Download/WindowsServer.iso"
-    [String] $isopath = "/Volumes/VM_SSD/17763.1158.200413-1759.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_ja-jp.iso"
+    [String] $isopath = 
 }
 
