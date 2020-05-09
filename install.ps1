53c08ffdd400d8e6bae93026a1634af9c1f36aef
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2020-05-09 19:52:43 +0900

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -11,13 +11,14 @@ VBoxManage modifyvm $config.VMName --vram $config.VMRam
 
 VBoxManage modifyvm $config.VMName --cpus $config.VMCpus
 
-VBoxManage createhd --filename "$($config.VMName)/$($config.VMName).vdi" --size $config.VDISize --format VDI
+VBoxManage createhd --filename "~/VirtualBox VMs/$($config.VMName)/$($config.VMName).vdi" --size $config.VDISize --format VDI
 
 VBoxManage storagectl $config.VMName --name "SATA Controller" --add sata --controller IntelAhci
 
-VBoxManage storageattach $config.VMName --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$($config.VMName)/$($config.VMName).vdi"
+VBoxManage storageattach $config.VMName --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "~/VirtualBox VMs/$($config.VMName)/$($config.VMName).vdi"
 
 VBoxManage storagectl $config.VMName --name "IDE Controller" --add ide --controller PIIX4
 
 VBoxManage storageattach $config.VMName --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium $config.isopath
 
+VBoxManage sharedfolder add $config.VMName --name $config.VMShareName --hostpath "./share"
