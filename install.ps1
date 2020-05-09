ca94f53fa76d666a0643dc099a84ff9f923885db
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2020-05-09 14:36:53 +0900

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,8 @@
+VBoxManage modifyvm "WindowsServer" --memory 8192
+VBoxManage modifyvm "WindowsServer" --vram 256
+VBoxManage modifyvm "WindowsServer" --cpus 4
+VBoxManage createhd --filename ~/VirtualBox\ VMs/WindowsServer/WindowsServer.vdi --size 80000 --format VDI
+VBoxManage storagectl WindowsServer --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach WindowsServer --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium VirtualBox\ VMs/WindowsServer/WindowsServer.vdi
+VBoxManage storagectl WindowsServer --name "IDE Controller" --add ide --controller PIIX4
+VBoxManage storageattach WindowsServer --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium /Volumes/VM_SSD/17763.1158.200413-1759.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_ja-jp.iso
